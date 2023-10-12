40cd4c5dd339cd9da9e02b7327e12003b033d133
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-10-12 19:31:57 +0200

    done-final-ex-pari/dispari/added-function-at-ex-1

--- js/app.js
+++ js/app.js
@@ -1,21 +1,27 @@
 
-// # js-palindroma
+// // # js-palindroma
 
-const word = prompt('Inserisci un parola').toUpperCase();
-console.log(word)
+// - chiedere al utente una parola
+const userWord = prompt('Inserisci un parola').toUpperCase();
+console.log(userWord)
 
-let reverseWord = '';
+function reverseWord(word) {
 
-for (let i = word.length -1; i >= 0; i--) {
-    const letters = word.charAt(i);
-    console.log(i, letters);
+    let reversedWord = '';
 
-    reverseWord += letters;
+    for (let i = word.length -1; i >= 0; i--) {
+        const letters = word.charAt(i);
+        console.log(i, letters);
+        reversedWord += letters;
+    }
+
+    return reversedWord;
 }
 
-console.log(reverseWord);
-
-if (word === reverseWord) {
+const miaParola = reverseWord(userWord)
+console.log(miaParola)
+    
+if (userWord === miaParola) {
     console.log('la parola è palindroma');
 
 } else {
@@ -23,3 +29,32 @@ if (word === reverseWord) {
 }
 
 
+// # js- pari o dispari
+
+function generatorIntNum (){
+    // - creare una variabile e generare un numero random da 1 / 5
+    const genIntNum = Math.floor(Math.random() * (5) + 1);
+    return genIntNum;
+}
+
+// - creare una variabile ed assegnare prompt applicano parseInt 
+const userNumber = parseInt(prompt('Scegli un numero tra 1 a 5'));
+console.log(userNumber)
+
+// - SE il numero è ( > 5 || < 1) dare alert
+if (userNumber > 5 || userNumber < 1 ) {
+    alert('scegli un numero tra 1 a 5')
+}
+
+let pcNum = generatorIntNum();
+console.log('numero scelto dal pc ' + pcNum);
+
+const plusNum = pcNum + userNumber;
+console.log('somma totale ' + plusNum)
+
+if (plusNum % 2 === 0) {
+    console.log('il numero è pari')
+
+} else {
+    console.log('il numero è dispari')
+}
